11950d88dc780e5405c5375b73fc976f28c2470a
bhavinvala <55101367+bhavinvala@users.noreply.github.com>
2020-03-22 20:41:21 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -28,11 +28,10 @@ dt2$Voltage<-as.numeric(as.character(dt2$Voltage))
 dt2$Global_reactive_power<-as.numeric(as.character(dt2$Global_reactive_power))
 dt2$datetime<-strptime(paste(dt2$Date,dt2$Time),"%d/%m/%Y %H:%M:%S")
 
-#set the 2 row 2 column for plot, arrange margins;
-par(mfrow=c(2,2),mar=c(4,4,1,1))
-
 #Plot
 png(file="plot4.png",height=480,width=480,units="px");
+#set the 2 row 2 column for plot, arrange margins;
+par(mfrow=c(2,2),mar=c(4,4,1,1))
 #1-Topleft plot
 with(dt2,plot(datetime,Global_active_power,ylab="Global Active Power (kilowatts)",xlab=' ',type='n'))
 with(dt2,lines(datetime,Global_active_power))
